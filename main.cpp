9f6bf0e324b2b3153d22b4fcb320e6273bbd955f
Edgar David Vargas Fuentes <edgar981125@live.com.mx>
2022-04-23 18:40:19 -0500

    Se agregó en el archivo fuente main dos funciones void dónde solo se utilizan para visualizar los menús, se agregó la bibiloteca para caracteres especiales y enla función principal se implementó un do-while y un switch, pero solo las caparazones, aún sin opciones ya que no se han implementado en los otros archivos

--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <locale.h>
 using namespace std;
+void menu()
+{
+    cout << "\tMenú" << endl;
+    cout << "1) Agregar al inicio un Jugador" << endl;
+    cout << "2) Agregar al final un Jugador" << endl;
+    cout << "3) Insertar un Jugador por posición" << endl;
+    cout << "4) Muestra el primer jugador" << endl;
+    cout << "5) Muestra el último jugador" << endl;
+    cout << "6) Ordenar" << endl;
+    cout << "7) Eliminar un jugador al inicio" << endl;
+    cout << "8) Eliminar un jugador al final" << endl;
+    cout << "9) Eliminar un jugador por posición" << endl;
+    cout << "10) Buscar " << endl;
+    cout << "11) Modificar" << endl;
+    cout << "12) Mostrar el estado del arreglo" << endl;
+    cout << "13) Mostrar todos los jugadores del arreglo" << endl;
+    cout << "14) Salir" << endl;
+
+
+}
+void menu_ordenar()
+{
+    cout << "\tMenú Ordenar" << endl;
+    cout << "1) Ordena por Nickname (Ascendente)" << endl;
+    cout << "2) Ordena por Minutos jugados (Ascendente)" << endl;
+    cout << "3) Ordena por Puntuación (Descendente)" << endl;
+}
 int main()
 {
+     setlocale(LC_ALL, "spanish");
+     int opcion;
+    do
+    {
+        menu();
+        switch (opcion)
+        {
+        case 1:
+
+            break;
+        
+        default:
+            break;
+        }
+    } while (opcion != 14);
     
     return 0;
 }
