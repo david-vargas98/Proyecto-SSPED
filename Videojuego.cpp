2a5b61a1cee83f9d36d01d9d83d5690311a4c8bc
Edgar David Vargas Fuentes <edgar981125@live.com.mx>
2022-04-23 20:52:39 -0500

    Se creó la implementación de la clase VideoJuego

---
+++ Videojuego.cpp
@@ -0,0 +1,55 @@
+#include "Videojuego.hpp"
+//Constructor
+VideoJuego::VideoJuego()
+{
+    
+}
+//Métodos de acceso
+void agregarAlInicio()
+{
+
+}
+void agregarAlFinal()
+{
+
+}
+void insertarPorPosicion()
+{
+
+}
+void eliminarAlInicio()
+{
+
+}
+void eliminarAlFinal()
+{
+
+}
+void eliminarPorPosicion()
+{
+
+}
+void ordenarAscendenteNickname() //Usando una función lambda.
+{
+
+}
+void ordenarAscendenteMinutos() //Usando una función lambda.
+{
+
+}
+void ordenarDescendentePuntuacion()
+{
+    //Usando una función lambda.
+} 
+void buscarPorNickname()
+{
+
+}
+void buscarPorPuntuacionAlta() //El Jugador con la puntuacion más alta.
+{
+
+}
+void buscarPorMayorCantidadMinutos() //El Jugador con la cantidad de minutos jugados más alta.
+{
+
+}
