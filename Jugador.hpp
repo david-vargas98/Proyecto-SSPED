f63ca3591a7edec7d0304d61f440ed71dda3f976
Edgar David Vargas Fuentes <edgar981125@live.com.mx>
2022-04-25 11:36:41 -0500

    Se implementa el constructor y metodos de acceso en el .hpp

--- Jugador.hpp
+++ Jugador.hpp
@@ -9,15 +9,16 @@ class Jugador
     float minutosJugados;
     float puntuacion;
     public:
-    //Constructor
-    Jugador();
+    //Constructores
+    Jugador() = default;
+    Jugador(const string& nickname, float minutosJugados, float puntuacion):nickname{nickname}, minutosJugados{minutosJugados}, puntuacion{puntuacion}{}
     //Métodos de acceso
-    void setNickname(const string& valor);
-    string getNickname() const;
-    void setMinutosJugados(float valor);
-    float getMinutosJugados() const;
-    void setPuntuacion(float valor);
-    float getPuntuacion() const;
+    void setNickname(const string& valor){nickname = valor;}
+    string getNickname() const{return nickname;}
+    void setMinutosJugados(float valor){minutosJugados = valor;}
+    float getMinutosJugados() const{return minutosJugados;}
+    void setPuntuacion(float valor){puntuacion = valor;}
+    float getPuntuacion() const{return puntuacion;}
 };
 
 #endif
