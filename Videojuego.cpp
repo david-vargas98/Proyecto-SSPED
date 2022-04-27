4476470a8dde30a6f0899301378e63bf897fa87c
Edgar David Vargas Fuentes <edgar981125@live.com.mx>
2022-04-26 19:48:39 -0500

    Se agrego las funciones de agregar all inicio, final e insertar asi como la de mostraró

--- Videojuego.cpp
+++ Videojuego.cpp
@@ -2,15 +2,34 @@
 //Métodos de acceso
 void VideoJuego::agregarAlInicio()
 {
-
+    //Se crea una instancia de Jugador para almacenar el que se va a ingresar.
+    Jugador j;
+    //Se usa la sobrecarga de la clase jugador para capturar un jugador.
+    cin >> j;
+    //Se agrega al arreglo el personaje.
+    jugadores.push_front(j);
 }
 void VideoJuego::agregarAlFinal()
 {
-
+    //Se crea una instancia de Jugador para almacenar el que se va a ingresar.
+    Jugador j;
+    //Se usa la sobrecarga de la clase jugador para capturar un jugador.
+    cin >> j;
+    //Se agrega al arreglo el personaje.
+    jugadores.push_back(j);
 }
 void VideoJuego::insertarPorPosicion()
 {
-
+    //Se declara una variable para pasar la posición válida.
+    size_t pos;
+    //Se crea una instancia de Jugador para almacenar el que se va a ingresar.
+    Jugador j;
+    //Se usa la sobrecarga de la clase jugador para capturar un jugador.
+    cin >> j;
+    //Se agrega al arreglo el personaje.
+    cout << "¿En qué posición válida desea agregar al jugador?" << endl;
+    cin >> pos;
+    jugadores.insert(j, pos);
 }
 void VideoJuego::eliminarAlInicio()
 {
@@ -47,4 +66,8 @@ void VideoJuego::buscarPorPuntuacionAlta() //El Jugador con la puntuacion más a
 void VideoJuego::buscarPorMayorCantidadMinutos() //El Jugador con la cantidad de minutos jugados más alta.
 {
 
+}
+void VideoJuego::mostrarJugadores()
+{
+    jugadores.mostrar();
 }
