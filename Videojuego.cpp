e628d79c079d11a9baa98a0b96a527c8f1d153c4
Edgar David Vargas Fuentes <edgar981125@live.com.mx>
2022-04-27 11:06:01 -0500

    Se implementó las funciones eliminar; primer y último; ordenar

--- Videojuego.cpp
+++ Videojuego.cpp
@@ -33,27 +33,50 @@ void VideoJuego::insertarPorPosicion()
 }
 void VideoJuego::eliminarAlInicio()
 {
-
+    jugadores.pop_front();
 }
 void VideoJuego::eliminarAlFinal()
 {
-
+    jugadores.pop_back();
 }
 void VideoJuego::eliminarPorPosicion()
 {
-
+    //Se declara una variable para pasar la posición a eliminar.
+    size_t pos;
+    cout << "¿Cuál es la posición del Jugador que desea eliminar?" << endl;
+    cin >> pos;
+    jugadores.erase(pos);
+}
+void VideoJuego::primer()
+{
+    cout << "Primer Jugador:" << endl;
+    cout << jugadores.front();
+}
+void VideoJuego::ultimo()
+{
+    cout << "Último Jugador:" << endl;
+    cout << jugadores.back();
 }
 void VideoJuego::ordenarAscendenteNickname() //Usando una función lambda.
 {
-
+    //Se declara la función lambda:
+    auto cmp = [] (const Jugador& j1, const Jugador& j2) { return j1.getNickname() > j2.getNickname(); };
+    jugadores.sort(cmp, true); //Se manda el comparador y "true" indicando que será orden ascendente.
+    cout << "Se ha ordenado por Nickname de manera Ascendente" << endl;
 }
 void VideoJuego::ordenarDescendenteMinutos() //Usando una función lambda.
 {
-
+    //Se declara la función lambda:
+    auto cmp = [] (const Jugador& j1, const Jugador& j2) { return j1.getMinutosJugados() > j2.getMinutosJugados(); };
+    jugadores.sort(cmp, false);  //Se manda el comparador y "false" indicando que será orden descendente.
+    cout << "Se ha ordenado por Número de mminutos jugados de manera Descendente" << endl;
 }
-void VideoJuego::ordenarDescendentePuntuacion()
+void VideoJuego::ordenarDescendentePuntuacion()  //Usando una función lambda.
 {
-    //Usando una función lambda.
+    //Se declara la función lambda:
+    auto cmp = [] (const Jugador& j1, const Jugador& j2) { return j1.getPuntuacion() > j2.getPuntuacion(); };
+    jugadores.sort(cmp, false);
+    cout << "Se ha ordenado por Puntuación de manera Descendente" << endl;
 } 
 void VideoJuego::buscarPorNickname()
 {
