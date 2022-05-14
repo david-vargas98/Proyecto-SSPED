f718ce1eb37fbe39ad12746cfa20e3a725d4d763
Edgar David Vargas Fuentes <edgar981125@live.com.mx>
2022-05-13 20:43:21 -0500

    Se implementaron las opciones de agregar y eliminar

--- menu_aldeanos.hpp
+++ menu_aldeanos.hpp
@@ -3,6 +3,23 @@
 #include <iostream>
 #include "Civilizacion.hpp"
 using namespace std;
+void eliminar()
+{
+    cout << "\tEliminar Aldeanos" << endl;
+    cout << "1) Eliminar por Nombre" << endl;
+    cout << "2) Eliminar Aldeanos dónde su salud sea menor a la que ingrese el ususario" << endl;
+    cout << "3) Eliminar aldeanos donde su edad sea mayor igual a 60" << endl;
+    cout << "4) Salir" << endl;
+    cout << "Selecciona una opción: ";
+}
+void agregar()
+{
+    cout << "\tAgregar Aldeanos" << endl;
+    cout << "1) Agregar al inicio" << endl;
+    cout << "2) Agregar al final" << endl;
+    cout << "3) Salir" << endl;
+    cout << "Selecciona una opción: ";
+}
 void menu_aldeanos_op(string s)
 {
     cout << "\tMenú Aldeanos para la Civilización " << s << endl;
@@ -16,7 +33,7 @@ void menu_aldeanos_op(string s)
     cout << "Seleciona una opción: ";
 }
 
-void menu_aldeanos(Civilizacion* c)
+void menu_aldeanos(Civilizacion* c, Jugador* j)
 {
     while (true)
     {
@@ -25,11 +42,83 @@ void menu_aldeanos(Civilizacion* c)
         cin >> opcion_aldeanos;
         if (opcion_aldeanos == 1)
         {
-            /* code */
+            //Creación del objeto de clase Aldeano.
+            Aldeano a;
+            //Se le asignan los atributos a el aldeano.
+            cin >> a;
+            //Variable para el sub-menú.
+            size_t opc;
+            //Se muestra el sub-menú.
+            agregar();
+            cin >> opc;
+            if (opc == 1)
+            {
+                c->agregarInicio(a);
+                //Se le agrega 100 puntos al puntaje del jugador.
+                float nueva;
+                nueva = j->getPuntuacion() + 100;
+                j->setPuntuacion(nueva);
+                cout << "Han sido añadidos " << 100 << " puntos al Jugador " << j->getNickname() << endl;
+            }
+            else if (opc == 2)
+            {
+                c->agregarFinal(a);
+                //Se le agrega 100 puntos al puntaje del jugador.
+                float nueva;
+                nueva = j->getPuntuacion() + 100;
+                j->setPuntuacion(nueva);
+                cout << "Han sido añadidos " << 100 << " puntos al Jugador " << j->getNickname() << endl;
+            }
+            else if (opc == 3)
+            {
+                cout << "Usted ha regresado al menú de Aldeanos" << endl;
+                break;
+            }
+            else if (opc < 1 || opc > 3)
+            {
+                cout << "Ha seleccionado una opción no válida" << endl;
+            }
+            
         }
         if (opcion_aldeanos == 2)
         {
-            /* code */
+            if (c->empty() == true)
+            {
+                cout << "No se puede eliminar ya que no hay Aldeanos agregados" << endl;
+            }
+            else
+            {
+                while (true)
+                {
+                    eliminar();
+                    size_t eliminar;
+                    cin >> eliminar;
+                    if (eliminar == 1)
+                    {
+                        c->eliminarNombre();
+                        break;
+                    }
+                    if (eliminar == 2)
+                    {
+                        c->eliminarSalud();
+                        break;
+                    }
+                    if (eliminar == 3)
+                    {
+                        c->eliminarEdad();
+                        break;
+                    }
+                    if (eliminar == 4)
+                    {
+                        cout << "Usted ha regresado al menú de Aldeanos" << endl;
+                        break;
+                    }
+                    if (eliminar < 1 || eliminar > 4)
+                    {
+                        cout << "Ha seleccionado una opción no válida" << endl;
+                    }
+                }
+            }
         }
         if (opcion_aldeanos == 3)
         {
