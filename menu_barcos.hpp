9d9beee9805da977f0e75eadad938e61f2f66a33
Edgar David Vargas Fuentes <edgar981125@live.com.mx>
2022-05-18 18:20:53 -0500

    Se creo la clase menu_barcos y se hizo el menu de los mismos

---
+++ menu_barcos.hpp
@@ -0,0 +1,54 @@
+#ifndef MENU_BARCOS_HPP
+#define MENU_BARCOS_HPP
+#include <iostream>
+#include "Civilizacion.hpp"
+#include "Jugador.hpp"
+using namespace std;
+void menu_barcos_opc(string s)
+{
+    cout << "\tMenú Barcos para la civilización " << s << endl;
+    cout << "1) Agregar Barco" << endl;
+    cout << "2) Mostrar todos los Barcos" << endl;
+    cout << "3) Buscar Barcos" << endl;
+    cout << "4) Eliminar Barco" << endl;
+    cout << "5) Salir" << endl;
+    cout << "Seleciona una opción: ";
+}
+void menu_barcos(Civilizacion* c, Jugador* j)
+{
+    while (true)
+    {
+        menu_barcos_opc(c->getNombre());
+        size_t opcion_barcos;
+        cin >> opcion_barcos;
+        if (opcion_barcos == 1)
+        {
+            
+        }
+        if (opcion_barcos == 2)
+        {
+            
+        }
+        if (opcion_barcos == 3)
+        {
+            
+        }
+        if (opcion_barcos == 4)
+        {
+            
+        }
+        if (opcion_barcos == 5)
+        {
+            cout << "Usted ha regresado al menú de Civilizaciones" << endl;
+            break;
+        }
+        if (opcion_barcos < 1 || opcion_barcos > 5)
+        {
+            cout << "Ha seleccionado una opción no válida" << endl;
+        }
+        
+
+    }
+    
+}
+#endif
